448e579e9983c444505a5038b5cab3889a968e6b
truesrc <ctzi6mrdxfte@bk.ru>
2019-01-28 15:46:14 +0200

    Куча

--- src/main/java/csv/MainCSV.java
+++ src/main/java/csv/MainCSV.java
@@ -13,30 +13,20 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static java.lang.System.getProperty;
-import static java.util.Arrays.asList;
-import static java.util.Arrays.parallelSort;
+import static java.util.Collections.sort;
 import static java.util.Objects.isNull;
-import static java.util.stream.Collectors.groupingByConcurrent;
-import static java.util.stream.Collectors.toList;
 
 public class MainCSV {
-    private static final int LIM_IDs = 20;
-    private static final int LIM_PRDS = 1000;
+    private static final int LIM_IDs = 7;
+    private static final int LIM_PRDS = 30;
     private static String pathIn = getProperty("user.dir") + "/src/main/java/csv/csv_in/";
     private static String pathOut = getProperty("user.dir") + "/src/main/java/csv/csv_out/";
 
     public static void main(String[] args) {
-        if (!isNull(args)) {
-            pathIn = args[0];
-            if (args.length == 2)
-                pathOut = args[1];
-        }
         try (Stream<Path> paths = Files.walk(Paths.get(pathIn))) {
             Optional<List<Product>> reduce = paths
                     .filter(Files::isRegularFile)
@@ -57,12 +47,13 @@ public class MainCSV {
                     }).reduce((list1000_1, list1000_2) -> {
                         list1000_1.addAll(list1000_2);
                         return getProducts(list1000_1);
-
                     });
 
             Writer writer = new FileWriter(pathOut + "out.csv");
             StatefulBeanToCsv<Product> build = new StatefulBeanToCsvBuilder<Product>(writer).build();
-            build.write(reduce.get());
+            List<Product> products = reduce.get();
+            sort(products);
+            build.write(products);
             writer.close();
 
         } catch (IOException | CsvRequiredFieldEmptyException | CsvDataTypeMismatchException e) {
@@ -71,24 +62,32 @@ public class MainCSV {
     }
 
     private static List<Product> getProducts(List<Product> parse) {
-        return parse
-                .parallelStream()
-                .collect(groupingByConcurrent(Product::getID))
-                .values()
-                .parallelStream()
-                .map(listSameIds -> {
-                    if (listSameIds.size() > LIM_IDs) {
-                        Product[] productsArr = listSameIds.toArray(new Product[listSameIds.size()]);
-                        parallelSort(productsArr, Product::compareTo);
-                        return asList(productsArr).subList(0, LIM_IDs);
-                    } else return listSameIds;
-                }).reduce((listNoMore20_1, listNoMore20_2) -> {
-                    listNoMore20_1.addAll(listNoMore20_2);
-                    return listNoMore20_1;
-                }).get()
-                .parallelStream()
-                .sorted()
-                .limit(LIM_PRDS)
-                .collect(toList());
+        Comparator<Product> cmp = (c1, c2) -> Float.compare(c2.getPrice(), c1.getPrice());
+        Map<Integer, PriorityQueue<Product>> map = new HashMap<>();
+        for (int i = 0; i < parse.size(); i++) {
+            Product product = parse.get(i);
+            if (!map.containsKey(product.getID())) {
+                PriorityQueue<Product> integers = new PriorityQueue<>(cmp);
+                integers.add(parse.get(i));
+                map.put(product.getID(), integers);
+            } else {
+                PriorityQueue<Product> integers = map.get(product.getID());
+                integers.add(parse.get(i));
+                map.put(product.getID(), integers);
+                if (integers.size() > LIM_IDs) {
+                    integers.poll();
+                }
+            }
+        }
+        PriorityQueue<Product> priorityQueueM = new PriorityQueue<>(LIM_PRDS, cmp);
+        for (PriorityQueue<Product> values : map.values()) {
+            for (int i = 0; i < values.size(); ) {
+                priorityQueueM.add(values.poll());
+                if (priorityQueueM.size() > LIM_PRDS) {
+                    priorityQueueM.poll();
+                }
+            }
+        }
+        return new ArrayList<>(priorityQueueM);
     }
 }
