198873161cba32f29b8f6837c2be8797c53b3661
truesrc <ctzi6mrdxfte@bk.ru>
2019-01-28 09:10:18 +0200

    В итоге продукты вместо просто цен

--- src/main/java/csv/Solution.java
+++ src/main/java/csv/Solution.java
@@ -1,11 +1,9 @@
 package csv;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
-
-import static java.util.Arrays.stream;
-import static java.util.Comparator.reverseOrder;
 
 public class Solution {
 
@@ -14,23 +12,25 @@ public class Solution {
         Product[] arr = new Product[n];
         int k = 20;
         int m = 1000;
-        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
-        stream(arr).forEach(product -> {
-            if (!map.containsKey(product.getID())) {
-                PriorityQueue<Integer> integers = new PriorityQueue<>(reverseOrder());
-                integers.add(product.getPrice());
-                map.put(product.getID(), integers);
+        Comparator<Product> cmp = (c1, c2) -> Float.compare(c2.getPrice(), c1.getPrice());
+        Map<Integer, PriorityQueue<Product>> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            Product Product = arr[i];
+            if (!map.containsKey(Product.getID())) {
+                PriorityQueue<Product> integers = new PriorityQueue<>(cmp);
+                integers.add(arr[i]);
+                map.put(Product.getID(), integers);
             } else {
-                PriorityQueue<Integer> integers = map.get(product.getID());
-                integers.add(product.getPrice());
-                map.put(product.getID(), integers);
+                PriorityQueue<Product> integers = map.get(Product.getID());
+                integers.add(arr[i]);
+                map.put(Product.getID(), integers);
                 if (integers.size() > k) {
                     integers.poll();
                 }
             }
-        });
-        PriorityQueue<Integer> priorityQueueM = new PriorityQueue<>(m, reverseOrder());
-        for (PriorityQueue<Integer> values : map.values()) {
+        }
+        PriorityQueue<Product> priorityQueueM = new PriorityQueue<>(m, cmp);
+        for (PriorityQueue<Product> values : map.values()) {
             for (int i = 0; i < values.size(); ) {
                 priorityQueueM.add(values.poll());
                 if (priorityQueueM.size() > m) {
@@ -38,6 +38,6 @@ public class Solution {
                 }
             }
         }
-    }
 
+    }
 }
